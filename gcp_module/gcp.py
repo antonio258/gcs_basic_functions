0a7d0aea92834dc03fff8c6c381541f2ee9c7a37
antonio258 <antonio258p@gmail.com>
2021-11-09 23:54:48 -0300

    add download folder

--- gcp_module/gcp.py
+++ gcp_module/gcp.py
@@ -17,9 +17,14 @@ def download(bucket_name, blob_path, down_path, project=''):
     storage_client = storage.Client(project=project)
     bucket = storage_client.get_bucket(bucket_name)
     blobs = bucket.list_blobs(prefix=blob_path)
+    blobs = [x for x in blobs]
     sep = '/' if down_path[-1] != '/' else ''
-    for blob in blobs:
-        os.makedirs(down_path + sep + blob.name.split('/')[:-1], exist_ok=True)
+    if len(blobs) > 1:
+        for blob in blobs:
+            os.makedirs(down_path + sep + blob.name.split('/')[:-1], exist_ok=True)
+            blob.download_to_filename(down_path)
+    else:
+        blob = bucket.blob(blob_path)
         blob.download_to_filename(down_path)
 
 
