b4e87def08e8ced0745b5cd21a297009ceb804f4
antonio258 <antonio258p@gmail.com>
2021-11-09 22:45:46 -0300

    Add folder upload

--- gcp_module/gcp.py
+++ gcp_module/gcp.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from google.cloud import storage
 
 
@@ -18,21 +20,26 @@ def download(bucket_name, blob_path, down_path, project=''):
     blob.download_to_filename(down_path)
 
 
-def upload(bucket_name, blob_path, file_path, project=''):
+def upload(bucket_name, blob_path, local_path, project=''):
     """
     Upload file to google storage
     Args:
         bucket_name: name of bucket
         blob_path: path of blob
-        file_path: local file path
+        local_path: local file path
         project: project name
 
     Returns:
     """
+    isdir = os.path.isdir(local_path)
     storage_client = storage.Client(project=project)
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(blob_path)
-    blob.upload_from_filename(file_path)
+    if isdir:
+        for file in glob(local_path + '/*'):
+            blob.upload_from_filename(blob_path + '/' + file)
+    else:
+        blob.upload_from_filename(local_path)
 
 
 def list_blobs(bucket_name, blob_path, project=''):
