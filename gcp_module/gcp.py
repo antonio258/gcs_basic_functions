28338c514e156c0cee7f450b2d218c091787d2ad
antonio258 <antonio258p@gmail.com>
2021-11-09 22:51:16 -0300

    Add folder upload

--- gcp_module/gcp.py
+++ gcp_module/gcp.py
@@ -37,7 +37,7 @@ def upload(bucket_name, blob_path, local_path, project=''):
     blob = bucket.blob(blob_path)
     if isdir:
         for file in glob(local_path + '/*'):
-            blob.upload_from_filename(blob_path + '/' + file.split('/')[-1])
+            blob.upload_from_filename(file)
     else:
         blob.upload_from_filename(local_path)
 
